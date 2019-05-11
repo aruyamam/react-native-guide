4bd2172687e7f5c329678d7087725c7e6b0cb1b6
aruyamam <aruyamam@gmail.com>
2019-05-11 13:54:36 +0900

    Setting Up the Reducer

--- rncourse/src/store/reducers/places.js
+++ rncourse/src/store/reducers/places.js
@@ -1,3 +1,5 @@
+import * as actionTypes from '../actions/acionTypes';
+
 const initialState = {
    places: [],
    selectedPlace: null
@@ -5,6 +7,44 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
    switch (action.type) {
+      case actionTypes.ADD_PLACE:
+         return {
+            ...state,
+            places: state.places.concat({
+               key: '' + Math.random(),
+               name: action.placeName,
+               image: {
+                  uri:
+                     'https://c1.staticflickr.com/5/4096/4744241983_34023bf303_b.jpg'
+               }
+            })
+         };
+
+      case actionTypes.DELETE_PLACE:
+         return {
+            ...state,
+            places: state.places.filter(
+               place => place.key !== state.selectedPlace.key
+            ),
+            selectedPlace: null
+         };
+
+      case actionTypes.SELECT_PLACE:
+         return {
+            ...state,
+            selectedPlace: state.places.find(
+               place => place.key === action.placeKey
+            )
+         };
+
+      case actionTypes.DESELECT_PLACE:
+         return {
+            ...state,
+            selectedPlace: null
+         };
+
+      default:
+         return state;
    }
 };
 
