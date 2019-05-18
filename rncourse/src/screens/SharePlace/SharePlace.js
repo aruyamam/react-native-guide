77e5361bb6f8d6798931a805ca885624c916d68c
aruyamam <aruyamam@gmail.com>
2019-05-18 13:55:06 +0900

    Adding a Tabs Screen

---
+++ rncourse/src/screens/SharePlace/SharePlace.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class SharePlace extends Component {
+   render() {
+      return (
+         <View>
+            <Text>On SharePlaceScreen</Text>
+         </View>
+      );
+   }
+}
+
+export default SharePlace;
